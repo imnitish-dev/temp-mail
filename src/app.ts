418d7547f987b100033e62c13194abc63b468f6d
nitish <imnitish@yahoo.com>
2024-04-22 08:13:36 +0530

    init

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import 'reflect-metadata';
+import express from 'express';
+import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from '@config';
+
+class App {
+    public app: express.Application;
+    public port: number;
+    public env: string;
+
+    constructor() {
+        this.app = express();
+        this.port = parseInt(PORT, 10);
+        this.env = NODE_ENV || 'development';
+    }
+
+    public listen() {
+        this.app.listen(this.port, () => {
+            if (this.env === 'development') {
+                console.log(`
+          .-----------------------------------.
+          |                                   |
+          |                                   |
+          |        ENV: ${this.env}           |
+          |  App listening on the port ${this.port}   |
+          |                                   |
+          |                                   |
+          |___________________________________|
+          `);
+            }
+        });
+    }
+}
+export default App;
---
+++ src/config/index.ts
@@ -0,0 +1,5 @@
+import { config } from 'dotenv';
+config({ path: `.env.${process.env.NODE_ENV || 'development'}` });
+export const { NODE_ENV, PORT, SECRET_KEY, LOG_FORMAT, LOG_DIR, ORIGIN } = process.env;
+export const CREDENTIALS = process.env.CREDENTIALS === 'true';
+export const { DB_HOST, DB_PORT, DB_DATABASE, DB_URL } = process.env;
