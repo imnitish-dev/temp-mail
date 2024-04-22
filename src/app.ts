8863b906786a47bb67fa1c6623382c028404225a
nitish <imnitish@yahoo.com>
2024-04-22 08:18:07 +0530

    added linting and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,22 @@
 import 'reflect-metadata';
 import express from 'express';
-import { NODE_ENV, PORT, LOG_FORMAT, ORIGIN, CREDENTIALS } from '@config';
+import { NODE_ENV, PORT } from '@config';
 
 class App {
-    public app: express.Application;
-    public port: number;
-    public env: string;
+  public app: express.Application;
+  public port: number;
+  public env: string;
 
-    constructor() {
-        this.app = express();
-        this.port = parseInt(PORT, 10);
-        this.env = NODE_ENV || 'development';
-    }
+  constructor() {
+    this.app = express();
+    this.port = parseInt(PORT, 10);
+    this.env = NODE_ENV || 'development';
+  }
 
-    public listen() {
-        this.app.listen(this.port, () => {
-            if (this.env === 'development') {
-                console.log(`
+  public listen() {
+    this.app.listen(this.port, () => {
+      if (this.env === 'development') {
+        console.log(`
           .-----------------------------------.
           |                                   |
           |                                   |
@@ -26,8 +26,8 @@ class App {
           |                                   |
           |___________________________________|
           `);
-            }
-        });
-    }
+      }
+    });
+  }
 }
 export default App;
