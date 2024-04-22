4e5c1858df3fd45c18cdbffb071857814d5557f8
nitish <imnitish@yahoo.com>
2024-04-22 11:49:02 +0530

    testing

--- src/controllers/mail.ts
+++ src/controllers/mail.ts
@@ -37,13 +37,41 @@ class MailController {
 
   public onRcptTo(address: SMTPServerAddress, session: SMTPServerSession, callback: () => void): void {
     console.log('onRcptTo', address, session);
+    /**
+     * onRcptTo { address: 'nitish@test-mail.imnitish.dev', args: false } {
+  id: 'xakpgvgasqno3oux',
+  secure: true,
+  localAddress: '172.31.4.122',
+  localPort: 25,
+  remoteAddress: '209.85.208.45',
+  remotePort: 57378,
+  clientHostname: 'mail-ed1-f45.google.com',
+  openingCommand: 'EHLO',
+  hostNameAppearsAs: 'mail-ed1-f45.google.com',
+  xClient: Map(0) {},
+  xForward: Map(0) {},
+  transmissionType: 'ESMTPS',
+  tlsOptions: {
+    name: 'TLS_AES_256_GCM_SHA384',
+    standardName: 'TLS_AES_256_GCM_SHA384',
+    version: 'TLSv1.3'
+  },
+  envelope: {
+    mailFrom: { address: 'nitish8040@gmail.com', args: false },
+    rcptTo: []
+  },
+  transaction: 1,
+  servername: 'test-mail.imnitish.dev'
+}
+     */
     callback();
   }
 
   public onData(stream: Readable, session: SMTPServerSession, callback: () => void): void {
-    console.log('onData', session);
-    stream.pipe(process.stdout);
-    stream.on('end', callback);
+    console.log('stream- onData=>', stream);
+   console.log(' session -onData=>', session);
+   stream.pipe(process.stdout);
+   stream.on('end', callback);
   }
 }
 
