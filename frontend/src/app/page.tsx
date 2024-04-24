f5c93a5fe377640319d49f953b19409c26e844f4
Anish <sharmaanish256@gmail.com>
2024-04-24 23:58:54 +0530

    basic functional frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,113 +1,63 @@
-import Image from "next/image";
+'use client';
+import Image from 'next/image';
+import { Dancing_Script } from 'next/font/google';
+import InboxView from '@/components/InboxView';
+import { useState } from 'react';
+const dancingScript = Dancing_Script({
+  weight: 'variable', // Specify the weights you need, e.g., 400, 500, 600, 700
+  display: 'swap',
+  subsets: ['latin'], // Recommended to avoid FOIT (Flash of Invisible Text)
+});
+
+const emailDomain = process.env.NEXT_PUBLIC_EMAIL_DOMAIN || '';
 
 export default function Home() {
+  const [currentEmail, setCurrentEmail] = useState<string>(`abc@${emailDomain}`);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    // Extract the username from the email
+    const username = event.target.value.split('@')[0];
+    setCurrentEmail(`${username}@${emailDomain}`);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex bg-[rgb(240,254,254)] text-black min-h-screen flex-col items-center justify-between p-24">
+      <section className="flex flex-col justify-center items-center">
+        <h1 className="text-6xl text-center text-[#343445]">
+          <div className={`${dancingScript.className} font-bold text-blue-600`}>Temporary</div> Email Address
+        </h1>
+        <div className="border-2 w-1/2 p-8 text-center border-white border-dotted">
+          <h3 className="p-4 text-xl">
+            Forget about spam, advertising mailings, hacking and attacking robots. Keep your real mailbox clean and secure. Temp Mail provides
+            temporary, secure, anonymous, free, disposable email address.
+          </h3>
+          <div className="flex justify-between px-4 gap-2 items-center">
+            <input value={currentEmail} onChange={handleChange} className="grow px-8 rounded-full bg-[#343445] text-white p-4"></input>
+            <button
+              onClick={() => {
+                navigator.clipboard.writeText(currentEmail);
+              }}
+              className="bg-[#EAF0F4] flex gap-2 rounded-full p-4 text-blue-500"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6 stroke-blue-500"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      </section>
+      {currentEmail != '' && <InboxView email={currentEmail} />}
     </main>
   );
 }
---
+++ frontend/src/components/InboxView.tsx
@@ -0,0 +1,112 @@
+'use client';
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+const InboxView = ({ email }: { email: String }) => {
+  const [data, setData] = useState(null);
+  const [countdown, setCountdown] = useState(5);
+  const [yourName, domain] = email.split('@');
+  // Function to fetch data
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`https://mailapi.imnitish.dev/v1/mail/${yourName}?page=1&limit=10`);
+      const data = await response.json();
+      setData(data);
+      // Reset the countdown whenever data is fetched
+      setCountdown(5);
+    } catch (error) {
+      console.error('Failed to fetch data:', error);
+    }
+  };
+
+  // Effect to fetch data on mount and set intervals
+  useEffect(() => {
+    fetchData(); // Fetch immediately on mount
+    const intervalId = setInterval(fetchData, 5000);
+
+    return () => clearInterval(intervalId); // Clean up the interval on unmount
+  }, [email]);
+
+  // Countdown timer
+  useEffect(() => {
+    if (countdown > 0) {
+      const timerId = setTimeout(() => setCountdown(countdown - 1), 1000);
+      return () => clearTimeout(timerId);
+    }
+  }, [countdown]);
+  return (
+    <div className="bg-[#F0F4FF] p-4 w-4/5 gap-4 rounded-lg flex flex-col ">
+      <button onClick={() => fetchData()}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+      </button>
+      <p>Next refresh in: {countdown} seconds</p>
+      <h2 className="text-xl">Your Inbox</h2>
+      {data && Array.isArray(data) ? (
+        <table className="text-left">
+          <thead>
+            <tr>{Object.keys(data[0]).map(key => key !== 'To' && <th key={key}>{key}</th>)}</tr>
+          </thead>
+          <tbody>
+            {data.map((row, i) => (
+              <tr key={i} className="w-min">
+                {Object.entries(row).map(
+                  ([key, value], j) => key !== 'To' && <td key={j}>{key === 'Date' ? new Date(value).toLocaleString() : value}</td>,
+                )}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <div className="p-8 w-full flex flex-col items-center justify-center gap-8">
+          <svg
+            height="200px"
+            width="200px"
+            version="1.1"
+            id="Capa_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 58 58"
+            xmlSpace="preserve"
+            fill="#000000"
+            className="h-24 w-24"
+          >
+            <g id="SVGRepo_bgCarrier" strokeWidth={0} />
+            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
+            <g id="SVGRepo_iconCarrier">
+              {'{'}' '{'}'}
+              <g>
+                {'{'}' '{'}'}
+                <path
+                  style={{ fill: '#0391FD' }}
+                  d="M25,9.586C11.193,9.586,0,19.621,0,32c0,4.562,1.524,8.803,4.135,12.343 C3.792,48.433,2.805,54.194,0,57c0,0,8.47-1.191,14.273-4.651c0.006-0.004,0.009-0.01,0.014-0.013 c1.794-1.106,3.809-2.397,4.302-2.783c0.301-0.417,0.879-0.543,1.328-0.271c0.298,0.181,0.487,0.512,0.488,0.86 c0.003,0.582-0.008,0.744-3.651,3.018c2.582,0.81,5.355,1.254,8.245,1.254c13.807,0,25-10.035,25-22.414S38.807,9.586,25,9.586z"
+                />
+                {'{'}' '{'}'}
+                <path
+                  style={{ fill: '#0F71D3' }}
+                  d="M58,23.414C58,11.035,46.807,1,33,1c-9.97,0-18.575,5.234-22.589,12.804 C14.518,11.153,19.553,9.586,25,9.586c13.807,0,25,10.035,25,22.414c0,4.735-1.642,9.124-4.437,12.743 C51.162,47.448,58,48.414,58,48.414c-2.805-2.805-3.792-8.566-4.135-12.657C56.476,32.217,58,27.976,58,23.414z"
+                />
+                {'{'}' '{'}'}
+                <path style={{ fill: '#FFFFFF' }} d="M32.5,26h-14c-0.552,0-1-0.447-1-1s0.448-1,1-1h14c0.552,0,1,0.447,1,1S33.052,26,32.5,26z" />
+                {'{'}' '{'}'}
+                <path style={{ fill: '#FFFFFF' }} d="M38,33H13c-0.552,0-1-0.447-1-1s0.448-1,1-1h25c0.552,0,1,0.447,1,1S38.552,33,38,33z" />
+                {'{'}' '{'}'}
+                <path style={{ fill: '#FFFFFF' }} d="M38,40H13c-0.552,0-1-0.447-1-1s0.448-1,1-1h25c0.552,0,1,0.447,1,1S38.552,40,38,40z" />
+                {'{'}' '{'}'}
+              </g>
+              {'{'}' '{'}'}
+            </g>
+          </svg>
+          Your inbox is empty
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InboxView;
